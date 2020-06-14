80c658c60c2c6761e2f854bf1cd9699a52f3731d
Nikhil Tyagi <nikhil.tyagi62@gmail.com>
2020-06-14 09:47:09 -0400

    Projects Page Complete

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,8 @@ import { Routes, RouterModule } from '@angular/router';
 const routes: Routes = [
   { path: '', loadChildren: () => import('./home/home.module').then(m => m.HomeModule) },
   { path: 'education', loadChildren: () => import('./education/education.module').then(m => m.EducationModule) },
-
+  { path: 'projects', loadChildren: () => import('./projects/projects.module').then(m => m.ProjectsModule) },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
