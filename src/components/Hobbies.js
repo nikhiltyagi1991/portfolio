e3fa36e0c2cb1064534c6ed0e3b15e557c150428
Nikhil Tyagi <nikhil.tyagi@teck.com>
2024-06-24 10:03:21 -0700

    Image load spinner fixed

--- src/components/Hobbies.js
+++ src/components/Hobbies.js
@@ -1,42 +1,37 @@
 import React from 'react';
 import portfolio from '../portfolio.json';
 import { Spinner } from '../icons';
+import ImageLoader from './ImageLoader';
 
 const Hobbies = () => {
     const [selectedArt, setSelectedArt] = React.useState(portfolio.artwork[0]);
-    const [loadingArray, setLoadingArray] = React.useState(portfolio.artwork.map(x => true));
-
-    const handleOnLoad = (idx) => {
-        console.log('image loaded');
-        setLoadingArray(Object.assign([], [...loadingArray], { idx: false }));
-    }
     return (
         <div>
             <div className='container'>
                 <div className='py-8'>
                     <div className='text-5xl text-center mb-8'>Hobbies & Artwork</div>
                     <div className="grid gap-4">
-                        <div>
-                            <img
-                                loading="lazy"
-                                className="h-[50vh] m-auto max-w-full rounded-lg"
-                                src={selectedArt}
-                                alt={portfolio.artwork} />
+                        <div className='w-full flex items-center'>
+                            <div className='m-auto max-w-full rounded-lg'>
+                                <ImageLoader
+                                    imgHeight='h-[50vh]'
+                                    src={selectedArt}
+                                    alt={selectedArt}
+                                    onClick={null}
+                                    isSelected={false}
+                                />
+                            </div>
                         </div>
                         <div className="flex gap-4 overflow-auto cursor-pointer rounded-sm min-w-fit">
                             {
-                                portfolio.artwork.map((x, idx) => (<div className="w-fit">
-                                    <Spinner key={x} style={{ display: loadingArray[idx] ? 'block' : 'none' }} />
-                                    <img
-                                        onClick={() => setSelectedArt(x)}
+                                portfolio.artwork.map((x) => (
+                                    <ImageLoader
+                                        imgHeight='h-32'
                                         key={x}
-                                        loading="lazy"
-                                        className={`h-32 ${x === selectedArt && 'border-gray-500 border-8'}`}
-                                        alt={x}
+                                        onClick={() => setSelectedArt(x)}
+                                        isSelected={selectedArt === x}
                                         src={x}
-                                        onLoad={() => { console.log('on load called'); handleOnLoad(idx); }}
-                                    />
-                                </div>))
+                                    />))
                             }
                         </div>
                     </div>
@@ -48,3 +43,17 @@ const Hobbies = () => {
 };
 
 export default Hobbies;
+
+
+
+{/* <div className="w-fit" key={x}>
+                                    <Spinner style={{ display: loadingArray[idx] ? 'block' : 'none' }} />
+                                    <img
+                                        onClick={() => setSelectedArt(x)}
+                                        loading="lazy"
+                                        className={`h-32 ${x === selectedArt && 'border-gray-500 border-8'}`}
+                                        alt={x}
+                                        src={x}
+                                        onLoad={() => { console.log('on load called'); handleOnLoad(idx); }}
+                                    />
+                                </div> */}
